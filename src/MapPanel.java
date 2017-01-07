483e1cd92e54c656e47a4c17eff4849b457a4b44
Ewa Janikowska <ewa.janikowska7@gmail.com>
2017-01-07 15:26:06 +0100

    Squashed commit of the following:
    commit a3e857858e2941fc870531462ec8a70e8edccd7e
    Author: Ewa Janikowska <ewa.janikowska7@gmail.com>
    Date:   Sat Jan 7 15:16:46 2017 +0100
    
        Add .gitignore, remove binaries and settings, moved files
    
    commit 1a8b1eddeaba4ed2888cb555cb13b32903c30420
    Author: MichalKucko <michak2304@gmail.com>
    Date:   Thu Jan 5 21:49:12 2017 +0100
    
        Add files via upload
    
        Pojawiaja sie mrowki i chodza losowo, az znajda jedzenie. Potem ida po gradiencie do mrowiska i znowu zaczynaja chodzic losowo.
        Legenda:
        bialy - mrowka bez jedzenia
        rozowy - mrowka z jedzeniem
        czerwony - jedzenie
        niebieski - mrowisko
        cyjanowy - gradient

--- src/MapPanel.java
+++ src/MapPanel.java
@@ -6,16 +6,38 @@ import jade.wrapper.StaleProxyException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Vector;
 
 public class MapPanel extends JPanel {
-    private static int CELL_H = 200;
-    private static int CELL_V = 200;
+    private static int CELL_H = 100;
+    private static int CELL_V = 100;
     private WorldCell[][] worldMap = new WorldCell[CELL_H][CELL_V];
+    private static int ANTHILL_CNT = 1;
+    static float MAX_GRADIENT = 100;
 
     public MapPanel(){
-        for (int i = 0; i < worldMap.length; ++i) {
-            for (int j = 0; j < worldMap[i].length; ++j) {
-                worldMap[i][j] = new WorldCell();
+        int anthills[][] = new int[ANTHILL_CNT][2];
+        for(int i = 0; i < ANTHILL_CNT; i++) {
+            anthills[i][0] = 2 + new Random().nextInt(getH() - 4);
+            anthills[i][1] = 2 + new Random().nextInt(getV() - 4);
+        }
+
+        float gradient, max_gradient;
+        for (int x = 0; x < worldMap.length; ++x) {
+            for (int y = 0; y < worldMap[x].length; ++y) {
+                max_gradient = 0;
+                int i;
+                for(i = 0; i < ANTHILL_CNT; i++) {
+                    if(Math.abs(anthills[i][0] - x) <= 2 && Math.abs(anthills[i][1] - y) <= 2) break;
+                    else {
+                        gradient = MAX_GRADIENT - (float)Math.hypot(x - anthills[i][0], y - anthills[i][1]);
+                        if(gradient > max_gradient) max_gradient = gradient;
+                    }
+                }
+                if(i == ANTHILL_CNT) worldMap[x][y] = new WorldCell(max_gradient / MAX_GRADIENT, false);
+                else worldMap[x][y] = new WorldCell(1, true);
             }
         }
     }
@@ -36,6 +58,15 @@ public class MapPanel extends JPanel {
     public WorldCell[][] getWorldMap(){
         return worldMap;
     }
+    public static int getH() {return CELL_H;}
+    public static int getV() {return CELL_V;}
+
+    public void setFood(int x, int y, boolean food) {worldMap[x][y].setFood(food);}
+    public void setAnt(int x, int y, int _ant) {
+        worldMap[x][y].setAnt(_ant);
+    }
+    public boolean isFood(int x, int y) {return worldMap[x][y].isFood();}
+    public float getGradient(int x, int y) {return worldMap[x][y].getGradient();}
 
 }
 
